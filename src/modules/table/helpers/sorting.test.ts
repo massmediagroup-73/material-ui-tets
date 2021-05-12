60d3dc2755d0ad0a955636af1d8aa05f49bacd1c
Yaroslav Kovalchuk <mmg.kovalchuk.yaroslav@gmail.com>
2021-05-12 09:21:57 +0300

    Add sorting test

--- src/modules/table/helpers/sorting.test.ts
+++ src/modules/table/helpers/sorting.test.ts
@@ -25,25 +25,25 @@ test('check sorting', () => {
     },
   ]
 
-  const expectsed: Row[] = [
+  const expected: Row[] = [
     {
-      name: 'John',
-      age: 20,
-    },
-    {
-      name: 'Bill',
-      age: 23,
+      name: 'Gary',
+      age: 33,
     },
     {
       name: 'Bob',
       age: 30,
     },
     {
-      name: 'Gary',
-      age: 33,
+      name: 'Bill',
+      age: 23,
+    },
+    {
+      name: 'John',
+      age: 20,
     },
   ]
   const sortOrder: SortOrder = 'desc'
   const sortOrderBy: keyof Row = 'age'
-  expect(stableSort(rows, getComparator(sortOrder, sortOrderBy))).toBe(expectsed)
+  expect(stableSort(rows, getComparator(sortOrder, sortOrderBy))).toMatchObject(expected)
 })
